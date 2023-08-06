dd7dfd75c7c009fe89daaba1786c5ccda4aaf976
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-08-06 09:25:56 -0400

    removed redundant files

--- C++/hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-std::string greet(){
-  return "hello world!";
-}
-
-//if you're reading this unironically in search of an actual solution to this problem, consider a book
-//on c++ for beginners. Tony Gaddis is a good start. 
--- Python/Hello_world.py
+++
@@ -1,5 +0,0 @@
-def greet():
-    return 'hello world!'
-
-#If you're reading this in order to actually solve that problem...please, just open a book for teaching
-#intro to python. I reccomend Tony Gaddis
